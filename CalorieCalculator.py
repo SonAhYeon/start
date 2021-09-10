a99f1b2d44654aaf475094896327715ca64171eb
SonAhYeon <89062378+SonAhYeon@users.noreply.github.com>
2021-09-10 17:35:50 +0900

    Initial commit

---
+++ CalorieCalculator.py
@@ -0,0 +1,21 @@
+"""
+1. 칼로리 계산기
+kikat : 190
+oreos : 502
+pringles : 292
+twix: 135.9
+cheetos : 485칼로리
+"""
+
+# 코드 작성
+kikat = 190
+oreos = 502
+pringles = 292
+twix = 135.9
+cheetos = 485
+
+# 출력 코드
+print(kikat + oreos * 2)
+print(cheetos * 4)
+print(pringles + oreos + twix)
+print(pringles * 3 + oreos * 2)
